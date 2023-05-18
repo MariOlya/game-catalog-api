c43e06eef56d2728d4e846bc4a475d13f624dbeb
Olga Marinina <youarewonderful@yandex.ru>
2023-05-18 22:02:51 +0700

    Add key for cookie validation

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'Y8_s8-6VrhNYFHLcQHigCwvITCMZUteq',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
